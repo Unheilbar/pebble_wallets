5807e20b1396cace06441fdd58c9fab41bcbf9ad
vladimir <vladimiramnell@gmail.com>
2023-10-13 03:19:55 +0300

    add main

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
+	"log"
+
 	"github.com/Unheilbar/pebbke_wallets/eth"
 	"github.com/Unheilbar/pebbke_wallets/node"
+	"github.com/Unheilbar/pebbke_wallets/raft"
 )
 
+var blockThrottle = time.Millisecond * 50
+
 func main() {
-	makeFullNode()
+	f, err := os.OpenFile(fmt.Sprintf("node%d.log", time.Now().Second()), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+	node, e := makeFullNode()
+	RegisterRaftService(node, e)
 }
 
 func makeFullNode() (*node.Node, *eth.Ethereum) {
@@ -14,4 +31,10 @@ func makeFullNode() (*node.Node, *eth.Ethereum) {
 	stack := node.New()
 
 	return stack, ethService
+}
+
+func RegisterRaftService(n *node.Node, e *eth.Ethereum) {
+	raft.NewRaft(e, blockThrottle)
+
+	log.Print("raft service registered")
 }
